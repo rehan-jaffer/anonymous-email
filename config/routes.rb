dc3f86604bdb96fc565d9be248ba1f1396303db7
Rehan <ray@thelondonvandal.com>
2013-10-04 16:05:56 +0100

    added paths for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Anonymail::Application.routes.draw do
 
   root 'home#index'
 
+  namespace :users do
+    
+    resources :dashboard, :only => [:index]
+
+  end
+
   namespace :api do
   
     resources :mail
