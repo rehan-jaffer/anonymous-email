ca0231f02ca07997180a387dfa9586fb5d439fad
Rehan <ray@thelondonvandal.com>
2013-10-04 16:07:56 +0100

    set up development to send mail to mailcatcher, enabling email confirmation of users in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,12 +15,13 @@ Anonymail::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:14000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.default_options = {:from => "admin@anonymail.com"}
+  config.action_mailer.smtp_settings = {:address => "127.0.0.1", :port => 1025}
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
