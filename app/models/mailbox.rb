e7347fe3c38d4fc96e3b6fd5606c6c43a0c92b92
Rehan <ray@thelondonvandal.com>
2013-10-05 13:56:22 +0100

    added debug logger for info on the exact format mandrill is sending in

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -5,12 +5,16 @@ class Mailbox
       alphabet = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
       mail_guid = (0...10).map{ alphabet[rand(alphabet.length)] }.join
 
+      StatusLogger.info(data.to_s)
+
       data.to_a.each do |datum|
+
+      StatusLogger.info(data.to_s)
 
         uid = datum["msg"]["to"].split("@")[0]
         REDIS.rpush("mailbox_#{uid}", mail_guid)
         REDIS.rpush("mail_queue", mail_guid)
-        REDIS.set("mail_#{mail_guid}", datum["msg"].to_json)      
+        REDIS.set("mail_#{mail_guid}", datum["msg"].to_json)    
 
      end
 
