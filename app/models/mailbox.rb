b3115cad2797604a035a593a7ccd8eac0e3d66ea
Rehan <ray@thelondonvandal.com>
2013-10-05 14:00:34 +0100

    heroku doesn't allow local custom logging, using the provided log stream instead

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -5,12 +5,11 @@ class Mailbox
       alphabet = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
       mail_guid = (0...10).map{ alphabet[rand(alphabet.length)] }.join
 
-      StatusLogger.info(data.to_s)
+      Rails.logger.info data.to_yaml
 
       data.to_a.each do |datum|
 
-      StatusLogger.info(data.to_s)
-
+        Rails.logger.info datum.to_yaml
         uid = datum["msg"]["to"].split("@")[0]
         REDIS.rpush("mailbox_#{uid}", mail_guid)
         REDIS.rpush("mail_queue", mail_guid)
