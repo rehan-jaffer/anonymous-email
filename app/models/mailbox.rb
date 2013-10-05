46f9bed26b0464705badf46dfb1ae28ec3254054
Rehan <ray@thelondonvandal.com>
2013-10-05 14:22:50 +0100

    setting up some debug code to check the right elements are extracted to be stored in redis

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -11,9 +11,17 @@ class Mailbox
 
         Rails.logger.info datum.to_yaml
         uid = datum["msg"]["to"][0][0].split("@")[0]
-        Rails.logger.info "UID ---- #{uid}"
         REDIS.rpush("mailbox_#{uid}", mail_guid)
         REDIS.rpush("mail_queue", mail_guid)
+
+        message_object = {}
+        message_object[:sender] = datum["from_email"]
+        message_object[:text] = datum["text"]
+        message_object[:html] = datum["html"]
+        message_object[:subject] = datum["subject"]
+
+        Rails.logger.info message_object.to_yaml
+
         REDIS.set("mail_#{mail_guid}", datum["msg"].to_json)    
 
      end
