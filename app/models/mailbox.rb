5880a7213e4ed478d61a9974d2c61b788314930b
Rehan <ray@thelondonvandal.com>
2013-10-05 14:27:27 +0100

    changed debug code to to_s rather than to_yaml, more readable in log files to figure out what's being delivered

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -5,11 +5,9 @@ class Mailbox
       alphabet = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
       mail_guid = (0...10).map{ alphabet[rand(alphabet.length)] }.join
 
-      Rails.logger.info data.to_yaml
-
       data.to_a.each do |datum|
 
-        Rails.logger.info datum.to_yaml
+        Rails.logger.info datum.to_s
         uid = datum["msg"]["to"][0][0].split("@")[0]
         REDIS.rpush("mailbox_#{uid}", mail_guid)
         REDIS.rpush("mail_queue", mail_guid)
