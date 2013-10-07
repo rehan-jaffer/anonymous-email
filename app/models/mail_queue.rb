50a23bae2b6779e92e0c2dad8cef339c46da0fea
Rehan <ray@thelondonvandal.com>
2013-10-07 14:45:01 +0100

    mail queue should now clear the queue on successful mailing

--- app/models/mail_queue.rb
+++ app/models/mail_queue.rb
@@ -7,12 +7,23 @@ class MailQueue
   def self.dispatch
     entries = REDIS.llen("mail_queue")
     queue = REDIS.lrange("mail_queue", 0, entries)
+
+    report = {sends: 0, failures: 0}
     
     queue.each do |item|
       
       mail = REDIS.hgetall("mail_#{item}")
-      mailer = Remailer.remail(mail).deliver
-      Rails.logger.info mail.to_yaml
+      begin
+        mailer = Remailer.remail(mail).deliver
+        Redis.lpop("mail_queue")
+        Redis.set("mail_#{item}", sent, 1)
+        report[:sends] += 1
+      rescue
+        report[:failures] += 1
+        Redis.lpush("errors", item)
+      end
+
+      Rails.logger.info report.to_yaml
 
     end
 
