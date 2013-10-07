95babf775d214e8c623381bdf079a5b6109ff62a
Rehan <ray@thelondonvandal.com>
2013-10-07 12:44:42 +0100

    remailer values should use strings as keys, not symbols

--- app/mailers/remailer.rb
+++ app/mailers/remailer.rb
@@ -2,8 +2,8 @@ class Remailer < ActionMailer::Base
   default from: "remailer@personal.bosonstudios.com"
 
   def remail(mail_object)
-    mail(to: mail_object[:address], subject: mail_object[:subject]) do |format|
-              format.text { render text: mail_object[:text], layout: nil }
+    mail(to: mail_object["address"], subject: mail_object["subject"]) do |format|
+              format.text { render text: mail_object["text"], layout: nil }
     end
 
   end
