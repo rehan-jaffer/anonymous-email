c4274939af4cf9f8522799465fbd996f09186a8f
Rehan <ray@thelondonvandal.com>
2013-10-07 12:20:29 +0100

    corrected syntax error in remailer

--- app/mailers/remailer.rb
+++ app/mailers/remailer.rb
@@ -2,8 +2,7 @@ class Remailer < ActionMailer::Base
   default from: "remailer@personal.bosonstudios.com"
 
   def remail(mail_object)
-    mail(to: mail_object[:address], subject: mail_object[:subject])  
-    do |format|
+    mail(to: mail_object[:address], subject: mail_object[:subject]) do |format|
               format.html { render html: mail_object[:html] }
               format.text { render text: mail_object[:text] }
     end
