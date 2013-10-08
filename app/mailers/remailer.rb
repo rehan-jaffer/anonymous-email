6556fc8d64859387caf4ac9a0eeca18a226adf98
Rehan <ray@thelondonvandal.com>
2013-10-08 15:07:36 +0100

    remailer should use new templates

--- app/mailers/remailer.rb
+++ app/mailers/remailer.rb
@@ -2,9 +2,10 @@ class Remailer < ActionMailer::Base
   default from: "remailer@personal.bosonstudios.com"
 
   def remail(mail_object)
+    @content = mail_object["html"]
     mail(to: mail_object["address"], subject: mail_object["subject"]) do |format|
               format.text { render text: mail_object["text"], layout: nil }
-              format.html { render html: mail_object["html"] }
+              format.html { render html: @content }
     end
 
   end
