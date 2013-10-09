9293f6f92b5832109cd96330bb52d1cb7b37f6e6
Rehan <ray@thelondonvandal.com>
2013-10-09 13:37:04 +0100

    sender address syntax error needs debugging

--- app/mailers/remailer.rb
+++ app/mailers/remailer.rb
@@ -3,10 +3,9 @@ class Remailer < ActionMailer::Base
 
   def remail(mail_object, attachments)
     @content = mail_object["html"]
-    mail(from: mail_object["sender"], to: mail_object["address"], subject: mail_object["subject"]) do |format|
+    mail(to: mail_object["address"], subject: mail_object["subject"]) do |format|
               format.text { render text: mail_object["text"], layout: nil }
               format.html { render html: @content }
-
     
     end
 
