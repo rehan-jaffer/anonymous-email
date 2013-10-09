9d0e85c3a9ee904bc05ed52fb5fe466c9bf257e3
Rehan <ray@thelondonvandal.com>
2013-10-09 12:54:54 +0100

    added RedisPagination config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,7 @@
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
+RedisPagination.configure do |configuration|
+  configuration.redis = REDIS
+  configuration.page_size = 10
+end
