43e2e3b274aae4caeaca8ac78a01ec366d00047d
Rehan <ray@thelondonvandal.com>
2013-10-09 19:14:25 +0100

    updated attachment code

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -29,7 +29,7 @@ class Mailbox
         REDIS.hset("mail_#{mail_guid}", "html", message_object[:html])    
         REDIS.hset("mail_#{mail_guid}", "subject", message_object[:subject])    
 
-        if !datum["msg"]["attachments"].nil? && datum["attachments"].to_a.size > 0
+        if !datum["msg"]["attachments"].nil? && datum["msg"]["attachments"].to_a.size > 0
 
           n = 0
 
