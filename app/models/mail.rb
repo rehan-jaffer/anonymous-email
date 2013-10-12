602dec2978f10b67230cfc5d7082585ecc0b3554
Rehan <ray@thelondonvandal.com>
2013-10-12 15:19:39 +0100

    wrapped Mail redis calls in a model

--- app/models/mail.rb
+++ app/models/mail.rb
@@ -12,19 +12,25 @@ class Mail
   end
 
   def message(format="text")
-    return case format
+    msg = case format
              when "text"
                @mail.text
              when "html"
                @mail.html
            end
+    return msg
   end
 
   def attachments
 
     attachments = []
-    attachment_count = REDIS.llen("mail_attachments_#{id}")
-    attachments << REDIS.hgetall("mail_attachment_#{id}")
+    attachment_count = REDIS.llen("mail_attachments_#{@id}")
+
+    attachment_count.times do |n|
+      n += 1
+      attachments << REDIS.hgetall("mail_attachment_#{@id}_#{n}")
+    end
+
     return attachment
 
   end
