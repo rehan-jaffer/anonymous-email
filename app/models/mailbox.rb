427be634870b77920b60e29b2733dd9c7d07d1d1
Rehan <ray@thelondonvandal.com>
2013-10-12 12:47:41 +0100

    removed debug code to disable attachments

--- app/models/mailbox.rb
+++ app/models/mailbox.rb
@@ -35,7 +35,7 @@ class Mailbox
         REDIS.hset("mail_#{mail_guid}", "html", message_object[:html])    
         REDIS.hset("mail_#{mail_guid}", "subject", message_object[:subject])    
 
-        if !datum["msg"]["attachments"].nil? && datum["msg"]["attachments"].to_a.size > 0 && 1 == 0
+        if !datum["msg"]["attachments"].nil? && datum["msg"]["attachments"].to_a.size > 0
 
           n = 0
 
