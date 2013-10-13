3c647c51423b59c1b0b6b49e035ba292b6433465
Rehan <ray@thelondonvandal.com>
2013-10-13 17:54:34 +0100

    updated demo page

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -23,9 +23,9 @@ $(function() {
   $(".mailbox-form").on("ajax:complete", function(e, xhr, settings, exception) {
     mail = xhr.responseJSON;
 
-    mail_template = "<table class = 'col-md-8'><thead><th class = 'col-md-4'>From</th><th class = 'col-md-4'>Subject</th></thead>"
+    mail_template = "<table class = 'col-md-8'><thead><th class = 'col-md-2'>From</th><th class = 'col-md-2'>Subject</th><th class = 'col-md-2'>Has Attachments</th></thead>"
 
-    rows = _.map(xhr.responseJSON, function(element, index, list) { return '<tr><td class = "col-md-4">' + element["sender"] + '</td><td class = "col-md-4">' + element["subject"] + '</td></tr>' } ).join("\r\n")
+    rows = _.map(xhr.responseJSON, function(element, index, list) { return '<tr><td class = "col-md-2">' + element["sender"] + '</td><td class = "col-md-2"><a class = "mail" data-guid = "' + element["uid"]  + '">' + element["subject"] + '</a></td><td><a href = "#">' + element["has_attachments"] + '</a></td></tr>' } ).join("\r\n")
 
     $("#mailbox").html(mail_template + rows)
   });
