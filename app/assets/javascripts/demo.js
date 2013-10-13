356aa7ed19ea2f11a0d0d6a81697c89949d7ef79
Rehan <ray@thelondonvandal.com>
2013-10-13 17:37:54 +0100

    updated demo page

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -6,10 +6,12 @@ $(function() {
 
   $(document).on("ajax:beforeSend", function(evt, request, settings) {
     $("#console").append("Starting request at " + settings.url + "\r\n")
+    $("#console").scrollTop(($("#console").scrollTop()+50))
   });
 
   $(document).on("ajax:complete", function(e, xhr, settings) {
     $("#console").append("Received: " + JSON.stringify(xhr.responseJSON) + "\r\n")
+    $("#console").scrollTop(($("#console").scrollTop()+300))
   });
 
   $(".demo-login-form").on("ajax:complete", function(e, xhr, settings, exception) {
@@ -23,12 +25,13 @@ $(function() {
 
     mail_template = "<table class = 'col-md-4'><thead><th class = 'col-md-2'>From</th><th class = 'col-md-2'>Subject</th></thead>"
 
-    rows = _.map(xhr.responseJSON, function(element, index, list) { return '<tr><td class = "col-md-2"><%= element["sender"] %></td><td class = "col-md-2"><%= element["subject"] %></td></tr>' } ).join("\r\n")
+    rows = _.map(xhr.responseJSON, function(element, index, list) { return '<tr><td class = "col-md-2">' + element["sender"] + '</td><td class = "col-md-2">' + element["subject"] + '</td></tr>' } ).join("\r\n")
 
     $("#mailbox").html(mail_template + rows)
   });
 
   $(".demo-account-form").on("ajax:complete", function(e, xhr, settings, exception) {
+      $("#account").html("");
     _.each(xhr.responseJSON, function(value, key, list) {
       $("#account").append("<h2>" + key + "</h2>\r\n" + "<p>" + value + "</p>\r\n");
     });
