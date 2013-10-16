710ca318758bc9d942eb5bcec03069d57194ab1b
Rehan <ray@thelondonvandal.com>
2013-10-16 16:26:37 +0100

    rolify user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :token_authenticatable
 
+  rolify
+
   validates_uniqueness_of :email
 
   after_create :add_uid
