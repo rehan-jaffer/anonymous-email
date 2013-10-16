538836e7c8b86cb52cd03c131f245850c4ac5e98
Rehan <ray@thelondonvandal.com>
2013-10-16 16:25:23 +0100

    added authentication routine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
+require 'digest/hmac'
+require 'digest/sha1'
+require 'base64'
+require 'openssl'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -11,7 +16,31 @@ class ApplicationController < ActionController::Base
 
   private
 
-   def check_sender_integrity
+   def auth_key
+     "DQ9S63FpFBFr5mCJiAXvtw"
+   end
+
+   def check_sender_integrity(data)
+
+       encoding_string = "http://bosonstudios.com:19999/api/mail"
+
+       params.except(:action, :controller).keys.sort.each do |key|
+          encoding_string += CGI.unescape(key.to_s)
+          encoding_string += params[key]
+        end
+
+       webhook_key = request.headers["X-Mandrill-Signature"]
+
+       digest = OpenSSL::Digest.new('sha1')
+       generated_signature = Base64.encode64(OpenSSL::HMAC.digest(digest, auth_key, encoding_string)).strip
+       Rails.logger.info webhook_key
+       Rails.logger.info generated_signature
+
+       if webhook_key == generated_signature
+         return true
+       else
+         raise MandrillAuthenticationError
+       end
 
    end
 
