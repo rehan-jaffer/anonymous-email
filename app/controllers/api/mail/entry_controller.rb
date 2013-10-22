74460d464799552eab6072dda2be030ec3ed4535
Rehan <ray@thelondonvandal.com>
2013-10-22 18:27:18 +0100

    renamed mail to entry controller

---
+++ app/controllers/api/mail/entry_controller.rb
@@ -0,0 +1,52 @@
+require 'redis'
+require 'json'
+
+class Api::Mail::EntryController < ApplicationController
+
+  respond_to :json
+
+  before_filter :authenticate_api_mail_user!, :except => [:create]
+
+  protect_from_forgery :except => [:create]
+
+  def create
+
+      begin
+
+        data = JSON.parse(params[:mandrill_events])
+
+        check_sender_integrity
+ 
+        Mailbox.add(data)
+
+        render :status => 200, :text => nil, :layout => nil
+
+      rescue SecurityError => e
+
+        Rails.logger.info e.inspect
+        render :status => 500, :text => e.inspect, :layout => nil
+
+      end
+
+     return
+
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+
+    if current_api_mail_user.has_role?("admin")
+      render json: User.get_mail(params[:id])
+    else
+      render json: current_api_mail_user.get_mail(params[:id])
+    end
+
+    return
+  end
+
+end
