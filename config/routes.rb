faab4900bfd21aff376a5c602a9bb6f9c038eb76
Rehan <ray@thelondonvandal.com>
2013-10-22 17:46:57 +0100

    changed 'mail' to 'entry'

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Anonymail::Application.routes.draw do
     resources :tokens, :only => [:create, :destroy]
     resources :mail, :only => [:create]
     resources :mailbox
-    resources :mail
+    resources :entry
     devise_for :user, :module => "devise", :controllers => { :sessions => "api/mail/sessions", :registrations => "api/mail/registrations" }
 
     resources :users do
